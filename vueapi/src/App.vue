ceae3ab2f253bbce06aff08fc2eb0a077dc5acb8
grety <snl1016@163.com>
2024-07-05 10:28:50 +0800

    u

--- vueapi/src/App.vue
+++ vueapi/src/App.vue
@@ -1,10 +1,45 @@
-<script setup>
+<script>
+    import { ref } from "vue"
+    export default {
+        setup(){
+            // 使用ref定义响应式变量
+            const msg = ref("Welcome to XiaMen");
+            const country = ref("China");
+
+            function changeMsg() {
+                msg.value = `Welcome to ${country.value}`;
+                console.log( msg.value);
+            };
+
+            const counter = ref(0);
+
+            function incrementCounter() {
+                counter.value++;
+            };
+
+            // 通过reactive定义响应式变量
+            //const obj = reactive({ name: "XiaMen", age: 20 });
+ 
+
+            return {
+                msg,country,changeMsg,counter,incrementCounter,               //...rest of your setup code...
+            };         
+        },
+        
+
+    };
 
 </script>
 
 <template>
     <div>
         <div>use vueapi</div>
+        <div>{{ msg }}</div>
+        <button @click="changeMsg">改变msg</button>
+
+        <div>{{ counter }}</div>
+        <button @click="incrementCounter">counter++ </button>
+
     </div>
   
 </template>
